49314a8b951ce4e5220d2a86d7f163d28dd37217
Kombobill <118278282+Kombobill@users.noreply.github.com>
2023-01-14 23:41:53 +0300

    Add files via upload

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,10 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{links.map(link=>{
+    return <a key = {link} href = {'#'+link}> {link} </a>
+  })}//wrapping in jsx fragments
+  </nav>;
 }
 
 export default NavBar;
